b3cb21bda6258e384d3b04a51fdf693fe9428bcb
Rachel Hamby <tnthreat33@gmail.com>
2023-10-31 20:11:28 -0400

    done

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,21 @@
 class Song:
-    pass
+    count = 0 
+    genres = []
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        self.add_song_to_count() 
+        self.add_to_genre(genre)
+
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+
+    @classmethod
+    def add_to_genre(cls, genre):
+        if genre not in cls.genres:
+            cls.genres.appends(genre)
+
+
